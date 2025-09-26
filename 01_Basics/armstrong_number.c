6f4052bfb1bf17aa0937b7c2ffa49f989f3b2703
Praneet <spraneet333@gmail.com>
2025-09-26 13:22:23 +0530

    Update armstrong_number.c

--- 01_Basics/armstrong_number.c
+++ 01_Basics/armstrong_number.c
@@ -13,21 +13,21 @@
 int count_digit(int n);
 int main()
 {
-int number,count,digit,power,sum=0;     //Declaration of variables to store values
+int number, count, digit, power, sum = 0;     //Declaration of variables to store values
 
 //Getting input from user
 printf("Enter your number: ");
-scanf("%d",&number);
+scanf("%d", &number);
 int temp = number;      //Storing the number in temp variable
 count = count_digit(number);    //Calling the function and storing value in count
 
 //calculating sum of power of digits
 while(number > 0)
 {
-    digit = number%10;
-    power=pow(digit,count);
-    sum+=power;         //
-    number/=10;
+    digit = number % 10;
+    power = pow(digit, count);
+    sum += power;         //
+    number /= 10;
 }
 
 //Checking Armstrong number or not
@@ -39,11 +39,11 @@ return 0;       //Program ends successfully
 }
 int count_digit(int n)      //Helper function to count number of digits
 {
-int count=0;
+int count = 0;
 while(n > 0)
 {
     count++;
-    n/=10;
+    n /= 10;
 }
 return count;
 }
