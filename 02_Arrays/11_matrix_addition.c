d34bcc19bb7f6cdf3aa19204160d78e8f548c535
Praneet <spraneet333@gmail.com>
2025-09-26 17:53:42 +0530

    Update 11_matrix_addition.c

--- 02_Arrays/11_matrix_addition.c
+++ 02_Arrays/11_matrix_addition.c
@@ -25,37 +25,37 @@
 int main()
 {
 //Getting the number of rows and columns from user
-int rows,col;
+int rows, col;
 printf("Enter number of rows: ");
-scanf("%d",&rows);
+scanf("%d", &rows);
 printf("Enter number of columns: ");
-scanf("%d",&col);
+scanf("%d", &col);
 
 //Getting matrix elements from user
 int arr_1[rows][col];
 printf("\nEnter elements of first matrix: \n");
-for(int i=0;i < rows;i++)
+for(int i = 0;i < rows;i++)
 {
-    for(int j=0;j < col;j++)
+    for(int j = 0;j < col;j++)
     {
     scanf("%d",&arr_1[i][j]);
     }
 }
 int arr_2[rows][col];
 printf("\nEnter elements of second matrix: \n");
-for(int i=0;i < rows;i++)
+for(int i = 0;i < rows;i++)
 {
-    for(int j=0;j < col;j++)
+    for(int j = 0;j < col;j++)
     {
-    scanf("%d",&arr_2[i][j]);
+    scanf("%d", &arr_2[i][j]);
     }
 }
 
 //Calculating the sum of two the matrices
 int arr_3[rows][col];
-for(int i=0;i < rows;i++)
+for(int i = 0;i < rows;i++)
 {
-    for(int j=0;j < col;j++)
+    for(int j = 0;j < col;j++)
     {
         arr_3[i][j] = arr_1[i][j] + arr_2[i][j];
     }
@@ -63,11 +63,11 @@ for(int i=0;i < rows;i++)
 
 //Printing the resultant matrix
 printf("\nResultant matrix (Addition):\n");
-for(int i=0;i < rows;i++)
+for(int i = 0;i < rows;i++)
 {
-    for(int j=0;j < col;j++)
+    for(int j = 0;j < col;j++)
     {
-        printf("%d  ",arr_3[i][j]);
+        printf("%d  ", arr_3[i][j]);
     }
     printf("\n");
 }
