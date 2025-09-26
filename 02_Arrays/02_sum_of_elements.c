ac27d8b559fa650a2f0ab4546d0dacde43e54fb3
Praneet <spraneet333@gmail.com>
2025-09-26 15:55:14 +0530

    Update 02_sum_of_elements.c

--- 02_Arrays/02_sum_of_elements.c
+++ 02_Arrays/02_sum_of_elements.c
@@ -18,21 +18,21 @@ int main()
 //Getting the size of the array from user
 int n;
 printf("Enter size of array: ");
-scanf("%d",&n);
+scanf("%d", &n);
 
-int arr[n],sum=0;       //Declaration of variables to store values
+int arr[n], sum=0;       //Declaration of variables to store values
 //Getting array elements from user
-printf("Enter %d elements: \n",n);
-for(int i=0;i < n;i++)
+printf("Enter %d elements: \n", n);
+for(int i = 0;i < n;i++)
 {
-    scanf("%d",&arr[i]);
+    scanf("%d", &arr[i]);
 }
 
 //Calculating the sum of array elements
-for(int i=0;i < n;i++)
+for(int i = 0;i < n;i++)
 {
-    sum+=arr[i];
+    sum += arr[i];
 }
-printf("Sum of array elements: %d",sum);
+printf("Sum of array elements: %d", sum);
 return 0;       //Program ends successfully
 }
