9b6f2eae86287aea626ce8327a85b11001a6d985
Praneet <spraneet333@gmail.com>
2025-09-26 13:00:35 +0530

    Update simple_calculator.c

--- 01_Basics/simple_calculator.c
+++ 01_Basics/simple_calculator.c
@@ -17,16 +17,16 @@
 
 int main()
 {
-int number_1,number_2;      //Variables to store numbers
+int number_1, number_2;      //Variables to store numbers
 
 //Getting input from user
 printf("Enter your 1st number: ");
-scanf("%d",&number_1);
+scanf("%d", &number_1);
 printf("Enter your 2nd number: ");
-scanf("%d",&number_2);
+scanf("%d", &number_2);
 char operation;
 printf("Enter the operation to perform like(+,-,*,/): ");
-scanf(" %c",&operation);
+scanf(" %c", &operation);
 int result=0;
 
 //Checking the users operation and calculating result
@@ -34,26 +34,26 @@ switch(operation)
 {
     case '+':
     {
-        result =number_1 + number_2;        //Addition
-        printf("%d + %d = %d",number_1,number_2,result);
+        result = number_1 + number_2;        //Addition
+        printf("%d + %d = %d", number_1, number_2, result);
         break;
     }
     case '-':
     {
-        result =number_1 - number_2;        //Subtraction
-        printf("%d - %d = %d",number_1,number_2,result);
+        result = number_1 - number_2;        //Subtraction
+        printf("%d - %d = %d", number_1, number_2, result);
         break;
     }
     case '*':
     {
-        result =number_1 * number_2;        //Multiplication
-        printf("%d * %d = %d",number_1,number_2,result);
+        result = number_1 * number_2;        //Multiplication
+        printf("%d * %d = %d", number_1, number_2, result);
         break;
     }
     case '/':
     {
-        result =number_1 / number_2;        //Division
-        printf("%d / %d = %d",number_1,number_2,result);
+        result = number_1 / number_2;        //Division
+        printf("%d / %d = %d", number_1, number_2, result); 
         break;
     }
     default:
